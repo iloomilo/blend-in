e0e149690fd8578e6e22da3f354d3f075cadb978
Ilcan Tiryaki <ilcantiryaki46@gmail.com>
2025-05-26 09:44:24 +0200

    feat: imprint page, fix: WebSocketMessage type

---
+++ pages/imprint.vue
@@ -0,0 +1,9 @@
+<template>
+    <AppGlassContainer class="w-[400px] p-4">
+        <h1 class="font-bold text-4xl mb-4">Imprint</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Id expedita praesentium magni. Qui eaque sunt perspiciatis 
+        praesentium beatae fugiat magni consectetur, 
+        accusamus quidem sint quisquam omnis similique quae esse quam.
+    </AppGlassContainer>
+</template>
