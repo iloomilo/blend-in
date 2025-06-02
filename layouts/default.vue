c79fdcbbd7d3d159aa44b63aec979d283d125f95
Ilcan Tiryaki <ilcantiryaki46@gmail.com>
2025-06-02 18:14:40 +0200

    fix: logo size

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,15 +3,15 @@
     class="relative min-h-screen background-svg rounded-xl text-white text-xl font-semibold"
   >
     <AppInfos class="absolute top-5" />
-    <main class="grid h-screen sm:h-[80vh] place-content-center">
+    <main class="grid h-screen sm:h-[90vh] place-content-center">
       <NuxtLink to="/">
         <NuxtImg
           src="/images/logo.png"
           alt="Blendin Logo"
-          sizes="150px sm:300px"
+          sizes="100px sm:250px"
           placeholder
           format="webp"
-          class="drop-shadow-2xl rounded-full mx-auto"
+          class="drop-shadow-2xl w-[100px] sm:w-[250px]  rounded-full mx-auto"
         />
       </NuxtLink>
       <slot />
